1991f49cfba6ee520363dc28bca8c559e9a874e0
JackONeill03 <B00155573@mytudublin.ie>
2025-05-02 00:59:43 +0100

    Updated Models

--- app/Models/Enrollment.php
+++ app/Models/Enrollment.php
@@ -2,10 +2,19 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Enrollment extends Model
 {
-    use HasFactory;
+    protected $fillable = ['student_id', 'course_id', 'enrollment_date'];
+
+    public function student()
+    {
+        return $this->belongsTo(Student::class);
+    }
+
+    public function course()
+    {
+        return $this->belongsTo(Course::class);
+    }
 }
